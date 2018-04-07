42d331a755f288ebb2d32a53a66f7152c60b5a23
stela <stella.plamenova@gmail.com>
2018-04-07 13:47:58 +0200

    node_modules in repo

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -5,7 +5,7 @@ var config_deploy = {
 	"author": "Random Jane",  // needed to recover data from server app
 	"experiment_id": "MentalChronometry", // needed to recover data from server app
 	"description": "A classic sognitive psychology experiment",
-	"deployMethod" : 'debug', // set deployment method; use one of 'debug', 'localServer', 'MTurk', 'MTurkSandbox', 'Prolific',
+	"deployMethod" : "debug", // set deployment method; use one of 'debug', 'localServer', 'MTurk', 'MTurkSandbox', 'Prolific',
 	
 	// optional fields
 	"contact_email": "someRandomJanesEmail@randomJoesAndJanes.love", // who to contact in case of trouble
@@ -15,7 +15,7 @@ var config_deploy = {
 
 // checks the config _deploy.deployMethod is MTurk or MTurkSandbox,
 // sets the submission url to MTukr's servers
-config_deploy.MTurk_server = config_deploy.deployMethod == 'MTurkSandbox' ?
+config_deploy.MTurk_server = config_deploy.deployMethod == "MTurkSandbox" ?
     "https://workersandbox.mturk.com/mturk/externalSubmit" : // URL for MTurk sandbox
     config_deploy.deployMethod == 'MTurk' ?
     "https://www.mturk.com/mturk/externalSubmit" : // URL for live HITs on MTurk
@@ -24,4 +24,5 @@ config_deploy.MTurk_server = config_deploy.deployMethod == 'MTurkSandbox' ?
 config_deploy.liveExperiment = config_deploy.deployMethod !== "debug";
 config_deploy.is_MTurk = config_deploy.MTurk_server !== "";
 console.log("runs on MTurk: " + config_deploy.is_MTurk);
+console.log("MTurk server: " + config_deploy.MTurk_server);
 console.log("live experiment: " + config_deploy.liveExperiment);
